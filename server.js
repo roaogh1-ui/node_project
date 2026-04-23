1f909e7abbfc6f46969bc38d665df61f7f37d0ea
roaogh1-ui <roaogh1@gmail.com>
2026-04-23 10:38:09 +0900

    DB연결기능 구현

--- server.js
+++ server.js
@@ -3,17 +3,24 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 
-const dataset = [
+//1. routes폴더 생성 -> user.js 파일 생성 (목록조회)
+//2. config 폴더 생성 -> db.js 파일 생성 
+//    .env 파일에 DB정보 기록
+
+const userRouter = require('./routes/user');
+
+const dataSet = [
     {"f_name":"딸기", "count":10},
     {"f_name":"멜론", "count":7},
     {"f_name":"망고", "count":2}
 ];
 
 app.use(cors());
+app.use("/users", userRouter);
 
 app.get("/api/list", (req, res)=>{
     console.log("요청이 들어옴...");
-    res.send(dataset);
+    res.send(dataSet);
 });
 
 app.listen(port, "0.0.0.0", ()=>{
